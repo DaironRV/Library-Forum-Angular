f3b53ded1f3977ebfb17ef001aaf49c714fa0ed4
JMCardenass <JMCardenas1807@gmail.com>
2023-08-18 12:21:44 -0500

    feat: register form

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegistrationComponent {
+  // Aquí iría la lógica de tu componente
+}
