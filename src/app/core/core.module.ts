711b33200f34b1b4f22af5ad186a5137c6815e0e
Victor <vimatoca050106@gmail.com>
2023-08-22 15:17:29 -0500

    feat [ src/core/-> core.module.ts ]

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './loader/loader.component';
+import { ServiceComponent } from './service/service.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoaderComponent,
+    ServiceComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
