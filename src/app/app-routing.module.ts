9fdaed717d97a069b8808431c96c0d74873f8d59
Daniel <dannyking07@outlook.es>
2023-08-24 01:40:02 -0500

    feat [src/app -> app-routing.module.ts]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistrationComponent } from './register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
 
 const routes: Routes = [
-  { path: 'registro', component: RegistrationComponent },
+
+  {path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
